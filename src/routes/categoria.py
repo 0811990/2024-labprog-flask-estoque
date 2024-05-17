a0721bc2cc24ebda6992fa0dbe8538609d3b465b
ALUNO <aluno@ifsp.edu.br>
2024-05-16 22:18:20 -0300

    alterações de editar e deletar da categoria

--- src/routes/categoria.py
+++ src/routes/categoria.py
@@ -15,3 +15,52 @@ def lista():
 
     return render_template('categoria/lista.jinja2',
                            rset=rset)
+
+@bp.route('/add', methods=['GET', 'POST'])
+@login_required
+def add():
+    from = NovoCategoriaForm()
+    if form.validade_on_submit():
+        categoria = Categoria()
+        categoria.nome = form.nome.data
+        db.session.add(categoria)
+        db.session.commit()
+        flash(f"Categoria '{form.nome.data}' adicionada")
+        return redirect(url_for('categoria.lista'))
+
+    return render_template('categoria/add_edit.jinja2',
+                           title= 'Nova Categoria',
+                           form=form)
+
+
+@bp.route('/edit/<uuid:id_categoria>', methods=['GET', 'POST'])
+@login_required
+def edit(id_categoria):
+    categoria = categoria.get_by_id(id_categoria)
+    if categoria is None:
+        flash(f"Categoria inexistende", category='warning')
+        return redirect(url_for('categoria.lista'))
+
+    form = EditCategoriaForm(request.values, obj=categoria)
+    if form.validate_on_submit():
+        categoria.nome = form.nome.data
+        db.session.commit()
+        flash("Categoria Alterada", category='success')
+        return redirect(url_for('categoria.lista'))
+
+    return render_template('categoria/add_edit.jinja2',
+                            title="Alterar categoria",
+                            form=form)
+
+@bp.route('/del/<uuid:id_categoria>', methods=['GET', 'POST'])
+@login_required
+def remove(id_categoria):
+    categoria = categoria.get_by_id(id_categoria)
+    if categoria is None:
+        flash(f"Categoria inexistende", category='warning')
+        return redirect(url_for('categoria.lista'))
+
+    db.session.delete(categoria)
+    db.session.commit()
+    flash(f"Categoria removida", category='success')
+    return redirect(url_for('categoria.lista'))
