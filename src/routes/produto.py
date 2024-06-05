1e5f9b2105f697f674f161afd0ee6ab4bd9d6103
ALUNO <aluno@ifsp.edu.br>
2024-06-05 20:53:50 -0300

    22/05 - add, edit, delet - OK

--- src/routes/produto.py
+++ src/routes/produto.py
@@ -1,10 +1,9 @@
 from base64 import b64encode
 
-from flask import Blueprint, render_template, redirect, url_for, flash, request
+from flask import Blueprint, render_template, redirect, url_for, flash, request, abort, Response
 from flask_login import login_required
 
 from src.forms.produto import ProdutoForm
-from src.models.categoria import Categoria
 from src.models.produto import Produto
 from src.modules import db
 
@@ -14,9 +13,9 @@ bp = Blueprint('produto', __name__, url_prefix="/produto")
 
 @bp.route('/add', methods=['GET', 'POST'])
 @login_required
-def add ():
+def add():
+    from src.models.categoria import Categoria
     if Categoria.is_empty():
-
         flash("Impossível adicionar produto. Adcione pelo menos uma categoria",
               category='warning')
         return redirect(url_for('categoria.add'))
@@ -34,12 +33,12 @@ def add ():
             produto.possui_foto = True
             produto.foto_base64 = (b64encode(request.files[form.foto.name].read()).
                                     decode('ascii'))
-            produto.foto_mime = request.files[form.foto.name].minetype
+            produto.foto_mime = request.files[form.foto.name].mimetype
         else:
             produto.possui_foto = False
             produto.foto_base64 = None
             produto.foto_mime = None
-        Categoria = Categoria.get_by_id(form.categoria.data)
+        categoria = Categoria.get_by_id(form.categoria.data)
         if categoria is None:
             flash("Categoria inexistente!", category='danger')
             return redirect(url_for('produto.add'))
@@ -51,3 +50,20 @@ def add ():
 
     return render_template('produto/add_edit.jinja2', form=form,
                            title="Adicionar novo produto")
+
+@bp.route('/imagem/<uuid:id_produto>', methods=['GET'])
+def imagem(id_produto):
+    produto = Produto.get_by_id(id_produto)
+    if produto is None:
+        return abort(404)
+    conteudo, tipo = produto.imagem
+    return Response(conteudo, mimetype=tipo)
+
+@bp.route('/thumbnail/<uuid:id_produto>/<int:size>', methods=['GET'])
+@bp.route('/thumbnail/<uuid:id_produto>', methods=['GET'])
+def thumbnail(id_produto, size=128):
+    produto = Produto.get_by_id(id_produto)
+    if produto is None:
+        return abort(404)
+    conteudo, tipo = produto.thumbnail(size)
+    return Response(conteudo, mimetype=tipo)
